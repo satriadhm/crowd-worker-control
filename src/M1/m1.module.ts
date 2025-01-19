0b9262573e8d6578f2fa839f46eba65bce1c6fa9
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2025-01-19 10:27:08 +0700

    fix

--- src/M1/m1.module.ts
+++ src/M1/m1.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { M1Service } from './services/m1.service';
 import { M1Resolver } from './m1.resolver';
 import { TasksModule } from 'src/tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Eligibility, EligibilitysSchema } from './models/eligibility';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    MongooseModule.forFeature([
+      { name: Eligibility.name, schema: EligibilitysSchema },
+    ]),
+  ],
   providers: [M1Resolver, M1Service],
 })
 export class M1Module {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SecurityMiddleware } from './middlewares/security.middlewares';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(new SecurityMiddleware().use);
 
-  await app.listen(3000);
+  await app.listen(configService.getValue('PORT'));
 }
 bootstrap();
